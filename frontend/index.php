df620815a05f1b94f11c8b6f5c7a7c61e1c8c912
dxt99 <ericlouis19x@gmail.com>
2021-11-05 14:25:51 +0800

    added image scaling

--- frontend/index.php
+++ frontend/index.php
@@ -18,7 +18,7 @@ $filepath = "images/" . $_FILES["file"]["name"];
 
 if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
 {
-echo "<img src=".$filepath.">";
+echo "<img src=".$filepath." height=480>";
 } 
 else 
 {
