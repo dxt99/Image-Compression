2c0e35eae9f276e4740d15d53047045d7e60c2a1
dxt99 <ericlouis19x@gmail.com>
2021-11-05 14:17:28 +0800

    image upload done:

---
+++ frontend/index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Image Compression</title>
+</head>
+<body>
+<p> Image Compression <br/> </p>
+<form action="index.php" enctype="multipart/form-data" method="post">
+Select image to compress:
+<input type="file" name="file"><br/>
+<input type="submit" value="Upload" name="Submit1"> <br/>
+
+
+</form>
+<?php
+if(isset($_POST['Submit1']))
+{ 
+$filepath = "images/" . $_FILES["file"]["name"];
+
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
+{
+echo "<img src=".$filepath.">";
+} 
+else 
+{
+echo "Error !!";
+}
+} 
+?>
+
+</body>
+</html>
